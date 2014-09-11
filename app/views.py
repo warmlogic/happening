6ba5225e354efa1e3ede9d4fc7c55235b74d7f3b
Matt Mollison <matt.mollison@gmail.com>
2014-09-11 16:24:12 -0700

    get activity near a dumb cluster

--- app/views.py
+++ app/views.py
@@ -5,14 +5,15 @@ import happening
 import jinja2
 import pdb
 import app.helpers.maps as maps
+import select_data as sd
 
 # db = mdb.connect(user="username",host="localhost",passwd="secretsecret",db="world_innodb",
 #     charset='utf8')
 
-@app.route('/')
-@app.route('/index')
-def splash():
-    return render_template('splash.html')
+# @app.route('/')
+# @app.route('/index')
+# def splash():
+#     return render_template('splash.html')
         
 # @app.route('/db')
 # def cities_page():
@@ -38,15 +39,55 @@ def splash():
 # 		cities.append(dict(name=result[0], country=result[1], population=result[2]))
 # 	return render_template('cities.html', cities=cities)
 	
-@app.route("/happening",methods=['GET'])
+# @app.route("/happening",methods=['GET'])
+@app.route('/')
+@app.route('/index')
 def happening_page():
-    user_location = request.args.get("origin")
-    lat,lon,full_add,data = maps.geocode(user_location)
-    clusters = happening.whatsHappening(lat,lon)
-    restaurants = []
-    for i in range(len(clusters['X'])):
-        restaurants.append(dict(lat=clusters['X'][i][0], long=clusters['X'][i][1], clusterid=clusters['labels'][i]))
-    return render_template('results.html',results=restaurants,user_lat = lat, user_long = lon, faddress = full_add, ncluster = clusters['n_clusters'])
+
+    # # request location from user
+    # user_location = request.args.get("origin")
+    # lat,lon,full_add,data = maps.geocode(user_location)
+
+    # this_lon, this_lat = sd.set_get_boundBox(area_str='bayarea')
+    # this_lon, this_lat = sd.set_get_boundBox(area_str='sf')
+    # this_lon, this_lat = sd.set_get_boundBox(area_str='fishwharf')
+    # this_lon, this_lat = sd.set_get_boundBox(area_str='embarc')
+    # this_lon, this_lat = sd.set_get_boundBox(area_str='att48')
+    # this_lon, this_lat = sd.set_get_boundBox(area_str='pier48')
+    # this_lon, this_lat = sd.set_get_boundBox(area_str='attpark')
+    # this_lon, this_lat = sd.set_get_boundBox(area_str='mission')
+    # this_lon, this_lat = sd.set_get_boundBox(area_str='sf_concerts')
+    # this_lon, this_lat = sd.set_get_boundBox(area_str='nobhill')
+    # this_lon, this_lat = sd.set_get_boundBox(area_str='mtview_caltrain')
+    # this_lon, this_lat = sd.set_get_boundBox(area_str='apple_flint_center')
+    # this_lon, this_lat = sd.set_get_boundBox(area_str='levisstadium')
+
+    area_str='attpark'
+    # area_str='apple_flint_center'
+    activity, diff_lon, diff_lat, user_lon, user_lat = happening.whatsHappening(area_str=area_str, tz='US/Pacific')
+    # activity, diff_lon, diff_lat, user_lon, user_lat = happening.whatsHappening(this_lon, this_lat, area_str='apple_flint_center', tz='US/Pacific')
+
+    # restaurants = []
+    # for i in range(len(clusters['X'])):
+    #     restaurants.append(dict(lat=clusters['X'][i][0], long=clusters['X'][i][1], clusterid=clusters['labels'][i]))
+    # return render_template('results.html',results=restaurants,user_lat = lat, user_long = lon, faddress = full_add, ncluster = clusters['n_clusters'])
+
+    # collect tweets from dataframe within radius X of lon,lat
+    unit = 'meters'
+    radius = 200
+    radius_increment = 50
+    radius_max = 1000
+    min_activity = 200
+    events = []
+    for i in range(len(diff_lon)):
+        print 'getting tweets from near: %.6f,%.6f' % (diff_lat[i],diff_lon[i])
+        nearby = sd.selectActivityFromPoint(activity,diff_lon[i],diff_lat[i],unit,radius,radius_increment,radius_max,min_activity)
+        # just pass in the first tweet for now
+        if nearby.shape[0] > 0:
+            events.append(dict(lat=nearby['latitude'][0], long=nearby['longitude'][0], clusterid=i, tweet=nearby['text'][0]))
+            # pdb.set_trace()
+    # return render_template('results.html',results=events,user_lat = lat, user_lon = lon, faddress = full_add, ncluster = clusters['n_clusters'])
+    return render_template('results.html',results=events,user_lat = user_lat, user_lon = user_lon)
     
 @app.route("/testmap")
 def test_maps_page():
