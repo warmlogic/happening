ad2d577b9fbfe43ef8b36af339612c62f544f98a
Matt Mollison <matt.mollison@gmail.com>
2014-09-10 23:49:58 -0700

    minor

--- app/views.py
+++ app/views.py
@@ -1,7 +1,7 @@
 from app import app
 from flask import render_template, request
 import pymysql as mdb
-import foodgroups
+import happening
 import jinja2
 import pdb
 import app.helpers.maps as maps
@@ -38,11 +38,11 @@ def splash():
 # 		cities.append(dict(name=result[0], country=result[1], population=result[2]))
 # 	return render_template('cities.html', cities=cities)
 	
-@app.route("/foodgroups",methods=['GET'])
-def food_groups_page():
+@app.route("/happening",methods=['GET'])
+def happening_page():
     user_location = request.args.get("origin")
     lat,lon,full_add,data = maps.geocode(user_location)
-    clusters = foodgroups.foodGroups(lat,lon)
+    clusters = happening.whatsHappening(lat,lon)
     restaurants = []
     for i in range(len(clusters['X'])):
         restaurants.append(dict(lat=clusters['X'][i][0], long=clusters['X'][i][1], clusterid=clusters['labels'][i]))
