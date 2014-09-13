1379d41250c6ff07c9677cbc0bc7c92c104131a2
Matt Mollison <matt.mollison@gmail.com>
2014-09-12 17:11:34 -0700

    plot clusters

--- app/views.py
+++ app/views.py
@@ -83,9 +83,13 @@ def happening_page():
     nbins = 50
     nclusters = 5
 
-    activity, diff_lon, diff_lat, user_lon, user_lat = happening.whatsHappening(area_str=area_str,\
+    activity, n_clusters, user_lon, user_lat = happening.whatsHappening(area_str=area_str,\
         nbins=nbins,nclusters=nclusters,\
         time_now=time_now, time_then=time_then, tz=tz)
+
+    # activity, n_clusters, user_lon, user_lat, diff_lon, diff_lat = happening.whatsHappening(area_str=area_str,\
+    #     nbins=nbins,nclusters=nclusters,\
+    #     time_now=time_now, time_then=time_then, tz=tz)
 
     # restaurants = []
     # for i in range(len(clusters['X'])):
@@ -93,25 +97,34 @@ def happening_page():
     # return render_template('results.html',results=restaurants,user_lat = lat, user_long = lon, faddress = full_add, ncluster = clusters['n_clusters'])
 
     # collect tweets from dataframe within radius X of lon,lat
-    unit = 'meters'
-    radius = 200
-    radius_increment = 50
-    radius_max = 1000
-    min_activity = 200
+    # unit = 'meters'
+    # radius = 200
+    # radius_increment = 50
+    # radius_max = 1000
+    # min_activity = 200
+    # clusCount = 0
+
     events = []
-    clusCount = 0
-    heatmap = False
-    for i in range(len(diff_lon)):
-        print 'getting tweets from near: %.6f,%.6f' % (diff_lat[i],diff_lon[i])
-        nearby = sd.selectActivityFromPoint(activity,diff_lon[i],diff_lat[i],unit,radius,radius_increment,radius_max,min_activity)
-        if nearby.shape[0] > 0:
-            clusCount += 1
-            print 'clusCount: %d' % (clusCount)
-            # # just pass in the first tweet for now
-            # events.append(dict(lat=nearby['latitude'][0], long=nearby['longitude'][0], clusterid=i, tweet=nearby['text'][0]))
-            for j in range(nearby.shape[0]):
-                events.append(dict(lat=nearby['latitude'][j], long=nearby['longitude'][j], clusterid=clusCount, tweet=nearby['text'][j]))
-    return render_template('results.html',results=events,user_lat = user_lat, user_lon = user_lon, ncluster=len(diff_lon), heatmap=heatmap)
+    heatmap = True
+
+    for i in range(activity.shape[0]):
+        # events.append(dict(lat=nearby['latitude'][j], long=nearby['longitude'][j], clusterid=clusCount, tweet=nearby['text'][j]))
+        events.append(dict(lat=activity['latitude'][i], long=activity['longitude'][i], clusterid=activity['clusterNum'][i], tweet=activity['text'][i]))
+    return render_template('results.html',results=events,user_lat = user_lat, user_lon = user_lon, ncluster=n_clusters, heatmap=heatmap)
+
+
+    # for i in range(len(diff_lon)):
+    #     print 'getting tweets from near: %.6f,%.6f' % (diff_lat[i],diff_lon[i])
+    #     nearby = sd.selectActivityFromPoint(activity,diff_lon[i],diff_lat[i],unit,radius,radius_increment,radius_max,min_activity)
+    #     if nearby.shape[0] > 0:
+    #         clusCount += 1
+    #         print 'clusCount: %d' % (clusCount)
+    #         # # just pass in the first tweet for now
+    #         # events.append(dict(lat=nearby['latitude'][0], long=nearby['longitude'][0], clusterid=i, tweet=nearby['text'][0]))
+    #         for j in range(nearby.shape[0]):
+    #             # events.append(dict(lat=nearby['latitude'][j], long=nearby['longitude'][j], clusterid=clusCount, tweet=nearby['text'][j]))
+    #             events.append(dict(lat=nearby['latitude'][j], long=nearby['longitude'][j], clusterid=nearby['clusterNum'][j], tweet=nearby['text'][j]))
+    # return render_template('results.html',results=events,user_lat = user_lat, user_lon = user_lon, ncluster=len(diff_lon), heatmap=heatmap)
     
 @app.route("/testmap")
 def test_maps_page():
