ed53694e8f888cb17f39ea9341d7746b0bda159e
Matt Mollison <matt.mollison@gmail.com>
2014-09-15 20:14:17 -0700

    cleanup

--- app/views.py
+++ app/views.py
@@ -10,35 +10,6 @@ import pdb
 # db = mdb.connect(user="username",host="localhost",passwd="secretsecret",db="world_innodb",
 #     charset='utf8')
 
-# @app.route('/')
-# @app.route('/index')
-# def splash():
-#     return render_template('splash.html')
-        
-# @app.route('/db')
-# def cities_page():
-# 	with db: 
-# 		cur = db.cursor()
-# 		cur.execute("SELECT Name FROM city LIMIT 15;")
-# 		query_results = cur.fetchall()
-# 	cities = ""
-# 	for result in query_results:
-# 		cities += result[0]
-# 		cities += "<br>"
-# 	return cities
-	
-# @app.route("/db_fancy")
-# def cities_page_fancy():
-# 	with db:
-# 		cur = db.cursor()
-# 		cur.execute("SELECT Name, CountryCode, Population FROM city ORDER BY Population LIMIT 15;")
-# 
-# 		query_results = cur.fetchall()
-# 	cities = []
-# 	for result in query_results:
-# 		cities.append(dict(name=result[0], country=result[1], population=result[2]))
-# 	return render_template('cities.html', cities=cities)
-	
 # @app.route("/happening",methods=['GET'])
 @app.route('/')
 @app.route('/index')
