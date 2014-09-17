b8d33ea1c4f47cb25de280c7a0c2df5866f5a5fc
Matt Mollison <matt.mollison@gmail.com>
2014-09-16 18:40:33 -0700

    methods for doing NLP

--- select_data.py
+++ select_data.py
@@ -10,6 +10,7 @@ import matplotlib.pyplot as plt
 from sklearn.cluster import DBSCAN
 from sklearn.preprocessing import StandardScaler
 import pdb
+import re
 
 def compute_distance_from_point(lon1, lat1, lon2, lat2, unit='meters'):
     if unit == 'meters':
@@ -379,6 +380,57 @@ def clusterThose(activity_now,nbins,diffmore_lon,diffmore_lat,centerData=True,pl
 
     return activity_now, n_clusters_real, cluster_centers
 
+# modified from: http://ravikiranj.net/drupal/201205/code/machine-learning/how-build-twitter-sentiment-analyzer
+def processTweet(tweet):
+    # process the tweets
+    
+    #Convert to lower case
+    tweet = tweet.lower()
+    #Convert www.* or https?://* to URL
+    tweet = re.sub('((www\.[^\s]+)|(https?://[^\s]+))','URL',tweet)
+    #Convert @username to AT_USER
+    tweet = re.sub('@[^\s]+','AT_USER',tweet)
+    #Remove additional white spaces
+    tweet = re.sub('[\s]+', ' ', tweet)
+
+    #Replace #hashtag with hashtag
+    # tweet = re.sub(r'#([^\s]+)', r'\1', tweet)
+
+    # remove single quotes and multiple periods
+    tweet = tweet.replace('\'','').replace('..',' ')
+    return tweet
+
+def replaceTwoOrMore(s):
+    #look for 2 or more repetitions of character and replace with the character itself
+    # e.g., huuuuuuuungry
+    pattern = re.compile(r"(.)\1{1,}", re.DOTALL)
+    return pattern.sub(r"\1\1", s)
+
+def getFeatureVector(tweet,stop):
+    stop.append('AT_USER')
+    stop.append('URL')
+    stop.append('unicode_only')
+    stop.append('w')
+    stop.append('im')
+    stop.append('')
+
+    featureVector = []
+    #split tweet into words
+    words = tweet.split()
+    for w in words:
+        #replace two or more with two occurrences
+        w = replaceTwoOrMore(w)
+        #strip punctuation
+        w = w.strip('\'"?,.!@$%^&*()-_+=/:')
+        #check if the word stats with an alphabet
+        # val = re.search(r"^[a-zA-Z][a-zA-Z0-9]*$", w)
+        #ignore if it is a stop word
+        # if(w in stop or val is None):
+        if(w in stop):
+            continue
+        else:
+            featureVector.append(w)
+    return featureVector
 
 # if __name__ == '__main__':
 #     import plot_data
