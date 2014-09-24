0025b9510ebe217f62d77168f7a91c889f63594b
Matt Mollison <matt.mollison@gmail.com>
2014-09-23 21:13:23 -0700

    for if getting local database on MBA

--- authent.py
+++ authent.py
@@ -5,13 +5,13 @@ with open('dbauth.cfg','r') as f:
         dbauth[l.split('=')[0]]=l.split('=')[1]
 if 'port' in dbauth:
     dbauth['port']=int(dbauth['port'])
-dbauth_a={}
-with open('dbauth_a.cfg','r') as f:
+dbauth_m={}
+with open('dbauth_m.cfg','r') as f:
     lines=f.read().split('\n')
     for l in lines:
-        dbauth_a[l.split('=')[0]]=l.split('=')[1]
-if 'port' in dbauth_a:
-    dbauth_a['port']=int(dbauth_a['port'])
+        dbauth_m[l.split('=')[0]]=l.split('=')[1]
+if 'port' in dbauth_m:
+    dbauth_m['port']=int(dbauth_m['port'])
 twitauth={}
 with open('twitauth.cfg','r') as f:
     lines=f.read().split('\n')
